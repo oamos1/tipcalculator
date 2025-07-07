798868718671d857d4748d9e5d56df877105c982
Omolaja Amos <90118850+oamos1@users.noreply.github.com>
2025-07-07 18:02:27 -0400

    add more file

---
+++ tipcalculatorfolder/tests/test_calculator.py
@@ -0,0 +1,6 @@
+from tipcalculatorfolder.app.calculator import calculatorfolder 
+
+def tip_calculator_tip():
+    assert calculate_tip (100,10,2) == 55
+    assert calculate_tip (200, 15, 4) == 58
+    
