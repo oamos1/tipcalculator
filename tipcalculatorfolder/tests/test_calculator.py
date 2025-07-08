799dbce3138df8c1f0d9bd57d1f31e4fe932281e
Omolaja Amos <90118850+oamos1@users.noreply.github.com>
2025-07-07 22:12:37 -0400

    adding local file

--- tipcalculatorfolder/tests/test_calculator.py
+++ tipcalculatorfolder/tests/test_calculator.py
@@ -1,6 +1,5 @@
-from tipcalculatorfolder.app.calculator import calculatorfolder 
+from app.calculator import calculate_tip
 
-def tip_calculator_tip():
-    assert calculate_tip (100,10,2) == 55
-    assert calculate_tip (200, 15, 4) == 58
-    
+def test_calculate_tip():
+    assert calculate_tip(100, 10, 2) == 55
+    assert calculate_tip(200, 15, 4) == 58
